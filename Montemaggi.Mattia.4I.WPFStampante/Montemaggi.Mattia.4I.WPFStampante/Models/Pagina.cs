4e3ffce06ccfa1c51124c55ed0de03eaa02bbcf8
Mattia Montemaggi <67807749+goldyasd@users.noreply.github.com>
2023-12-03 15:26:43 +0100

    Initial commit

---
+++ Montemaggi.Mattia.4I.WPFStampante/Montemaggi.Mattia.4I.WPFStampante/Models/Pagina.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace Montemaggi.Mattia._4I.WPFStampante
+{
+    internal class Pagina
+    {
+        private int _C;
+        private int _M;
+        private int _Y;
+        private int _B;
+
+        public Pagina(int c, int m, int y, int b)
+        {
+            if (c < 4) _C = c;
+            if (m < 4) _M = m;
+            if (y < 4) _Y = y;
+            if (b < 4) _B = b;
+        }
+
+        public Pagina()
+        {
+            Random rnd = new Random();
+            _C = rnd.Next(0, 4);
+            _M = rnd.Next(0, 4);
+            _Y = rnd.Next(0, 4);
+            _B = rnd.Next(0, 4);
+        }
+
+        public int C { get => _C; set => _C = value; }
+        public int M { get => _M; set => _M = value; }
+        public int Y { get => _Y; set => _Y = value; }
+        public int B { get => _B; set => _B = value; }
+    }
+}
---
+++ Montemaggi.Mattia.4I.WPFStampante/Montemaggi.Mattia.4I.WPFStampante/Models/Stampante.cs
@@ -0,0 +1,85 @@
+﻿using System;
+
+namespace Montemaggi.Mattia._4I.WPFStampante
+{
+
+    enum Colore
+    {
+        Ciano,
+        Magenta,
+        Giallo,
+        Nero
+    }
+    internal class Stampante
+    {
+        private int _C = 100;
+        private int _M = 100;
+        private int _Y = 100;
+        private int _B = 100;
+        private int _Fogli = 200;
+
+        public bool Stampa(Pagina p)
+        {
+            if (p.B > _B || p.Y > _Y || p.C > _C || p.M > _M || _Fogli == 0)
+            {
+                return false;
+            }
+            else
+            {
+                _Fogli--;
+                _B -= p.B;
+                _C -= p.C;
+                _M -= p.M;
+                _Y -= p.Y;
+                return true;
+            }
+        }
+
+        public int StatoInchiostro(Colore c)
+        {
+            switch (c)
+            {
+                case Colore.Ciano:
+                    return _C;
+                case Colore.Magenta:
+                    return _M;
+                case Colore.Giallo:
+                    return _Y;
+                case Colore.Nero:
+                    return _B;
+                default:
+                    throw new Exception("Colore non trovato");
+            }
+        }
+
+        public int StatoCarta()
+        {
+            return _Fogli;
+        }
+
+        public void SostituisciColore(Colore c)
+        {
+            switch (c)
+            {
+                case Colore.Ciano:
+                    _C = 100;
+                    break;
+                case Colore.Magenta:
+                    _M = 100;
+                    break;
+                case Colore.Giallo:
+                    _Y = 100;
+                    break;
+                case Colore.Nero:
+                    _B = 100;
+                    break;
+            }
+        }
+
+        public void AggiungiCarta(int qta)
+        {
+            _Fogli += qta;
+        }
+
+    }
+}
